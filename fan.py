cb90512626cb02ee659b90c6cdc5025f6a8572cd
xuchaoji <admin@xuchaoji.com>
2019-09-08 16:53:49 +0800

    first commit

---
+++ fan.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2.7
+import RPi.GPIO as GPIO
+import time
+import commands
+
+# 温度高于T_HIGH风扇开始运行，低于T_LOW风扇关闭
+T_HIGH = 50
+T_LOW = 42
+fan_pin = 12
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(fan_pin, GPIO.OUT)
+
+def get_gpu_temp():
+    gpu_temp = commands.getoutput( '/opt/vc/bin/vcgencmd measure_temp' ).replace( 'temp=', '' ).replace( '\'C', '' )
+    return  float(gpu_temp)
+
+while 1:
+    gpu_temp_loop = get_gpu_temp()
+    print 'CPU temperature: ', gpu_temp_loop, ' C'
+    if gpu_temp_loop > T_HIGH:
+        GPIO.output(fan_pin, 0)
+    if gpu_temp_loop < T_LOW:
+        GPIO.output(fan_pin, 1)
+    time.sleep(1)
